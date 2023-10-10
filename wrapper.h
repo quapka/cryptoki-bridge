285e2e3c7d1147d44d747928e69387d22b2e91aa
Kristian Mika <kristian.mika@protonmail.com>
2023-10-10 12:14:16 +0200

    refactor: use stdcall for windows api

--- wrapper.h
+++ wrapper.h
@@ -1,13 +1,19 @@
 #define CK_PTR *
 
+#ifdef _WIN32
+#define STDCALL __stdcall
+#else
+#define STDCALL
+#endif
+
 #define CK_DECLARE_FUNCTION(returnType, name) \
-    returnType name
+    returnType STDCALL name
 
 #define CK_DECLARE_FUNCTION_POINTER(returnType, name) \
-    returnType(*name)
+    returnType(*STDCALL name)
 
 #define CK_CALLBACK_FUNCTION(returnType, name) \
-    returnType(*name)
+    returnType(*STDCALL name)
 
 #ifndef NULL_PTR
 #define NULL_PTR 0
